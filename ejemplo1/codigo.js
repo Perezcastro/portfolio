853cc887f29ac719d7995b8a1f5bfe279349ca0c
perezcastro <perezcastro@gmail.com>
2015-12-20 16:28:35 +0100

    testeo de canvas

--- ejemplo1/codigo.js
+++ ejemplo1/codigo.js
@@ -1,1 +1,21 @@
-alert("hola mundo");
+var dibujo, lienzo;
+
+function inicio()
+{
+	dibujo = document.getElementById("dibujito");
+	lienzo = dibujo.getContext("2d");
+
+	lienzo.moveTo(100,100);
+	lienzo.lineTo(200,100);
+	lienzo.lineTo(200,200);
+	lienzo.lineTo(100,200);
+	lienzo.lineTo(100,100);	
+	lienzo.strokeStyle= "#00FF00";
+	lienzo.stroke();
+	
+	lienzo.beginPath();
+	lienzo.strokeStyle= "#FF0000";
+	lienzo.arc(150,150,50,2*Math.PI,false);
+	lienzo.closePath();
+	lienzo.stroke();
+}
