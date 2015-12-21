cd2b3e5553c0e1cbf4ef1287ad2fb485eac3f6b3
perezcastro <perezcastro@gmail.com>
2015-12-21 11:02:23 +0100

    canvas terminado

--- ejemplo1/codigo.js
+++ ejemplo1/codigo.js
@@ -1,23 +1,23 @@
 var dibujo, lienzo;
 var t,b1,b2,b3;
-
 function inicio()
 {
 
 	t=document.getElementById("rayaUsuario");
-	b1=document.getElementById("dibujaRayas");
-	b1=document.getElementById("dibujaCirculo");
-	b1=document.getElementById("dibujaCuadrado");
+	b1=document.getElementById("botonRayas");
+	b2=document.getElementById("botonCirculo");
+	b3=document.getElementById("botonCuadrado");
 	dibujo = document.getElementById("dibujito");
 	lienzo = dibujo.getContext("2d");
 
+	b1.addEventListener("click",dibujaParrilla);
+	b2.addEventListener("click",dibujaCirculo);
+	b3.addEventListener("click",dibujaCuadrado);
 
-function dibujaParrilla(n)
-{
-	
 }
 
-
+function dibujaCuadrado()
+{
 	lienzo.moveTo(100,100);
 	lienzo.lineTo(200,100);
 	lienzo.lineTo(200,200);
@@ -25,10 +25,44 @@ function dibujaParrilla(n)
 	lienzo.lineTo(100,100);	
 	lienzo.strokeStyle= "#00FF00";
 	lienzo.stroke();
-	
+
+}
+
+function dibujaCirculo()
+{
 	lienzo.beginPath();
 	lienzo.strokeStyle= "#FF0000";
 	lienzo.arc(150,150,50,2*Math.PI,false);
 	lienzo.closePath();
 	lienzo.stroke();
+
+}
+
+function dibujaParrilla()
+{
+	var n;
+	var ancho,largo;
+
+	n=Number(t.value);
+	ancho = (300/n);
+	largo = (300/n);
+
+	lienzo.strokeStyle="#000000";
+
+
+	for(var linea=0;linea<n;linea++)
+	{
+		punto=linea*ancho;
+		
+		lienzo.moveTo(punto+0.5,0.5);
+		lienzo.lineTo(punto+0.5,300.5);
+		lienzo.stroke();
+
+		lienzo.moveTo(0.5,punto+0.5);
+		lienzo.lineTo(300.5,punto+0.5);
+		lienzo.stroke();
+		
+	}
+
+
 }
